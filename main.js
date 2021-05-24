39933b584ccb962a3de125467443784ec926c411
Omer Sabah <omerrj99@gmail.com>
2021-05-24 12:28:54 +0000

    Automatically backed up by Learn

--- main.js
+++ main.js
@@ -4,7 +4,7 @@ const FULL_HEART = '♥'
 
 // Your JavaScript code goes here!
 
-const glyphs=document.
+const glyphs=document.getElementsByClassName('like')
 const like=(e) =>{
   const heart = e.target;
   mimicServerCall("url")
