874b6532ae23913e7edc26974f14bcbc0049bcf4
Omer Sabah <omerrj99@gmail.com>
2021-05-24 12:20:50 +0000

    Automatically backed up by Learn

--- main.js
+++ main.js
@@ -4,7 +4,24 @@ const FULL_HEART = '♥'
 
 // Your JavaScript code goes here!
 
+const like=(e) =>{
+  const heart = e.target;
+  mimicServerCall("url")
+    .then(()=>{
+       heart.innerText = glyphStates[heart.innerText];
+       heart.style.color = colorStates[heart.style.color];
+    })
+    .catch(function(error) {
+      const modal = document.getElementById("modal");
+      modal.className = "";
+      modal.innerText = error;
+      setTimeout(() =>  modal.className = "hidden", 3000);
+    });
+}
 
+for (const glyph of articleHearts) {
+  glyph.addEventListener("click", likeCallback);
+}
 
 
 
